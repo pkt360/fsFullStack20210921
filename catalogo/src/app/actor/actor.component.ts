182ec00d01051d4bf63cbc17f10e197f944ea93f
pkt360 <lucomavi@gmail.com>
2021-11-02 19:25:39 +0100

    Catalogo

--- catalogo/src/app/actor/actor.component.ts
+++ catalogo/src/app/actor/actor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActorViewModelService } from './servicios.service';
 
 @Component({
   selector: 'app-actor',
@@ -7,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActorComponent implements OnInit {
 
-  constructor (protected vm: ActorViewModelService){
+  constructor (protected vm: ActorViewModelService){}
     public get VM(): ActorViewModelService{
       return this.vm;
     }
-  }
 
   ngOnInit(): void {
     this.vm.list();
   }
 
+  //@Component
 }
 
 
